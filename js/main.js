e9a99d167ea3e0aedcce566a696e9f68183eb83c
lambda1014 <i051121@naver.com>
2021-08-11 12:52:04 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-const searchEl = document.querySelector('.search')
-const searchInputEl = searchEl.querySelector('input')
-
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus();
-})
-
-searchInputEl.addEventListener('focus', function (){
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-})
-
-searchInputEl.addEventListener('blur', function (){
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-})
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 /* window는 우리가 보는 창 그 자체. 
@@ -147,6 +130,3 @@ spyEls.forEach(function (spyEl){
   .setClassToggle(spyEl, 'show') //show라는 클래스 넣기
   .addTo(new ScrollMagic.Controller());
 });
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); //올해 년도
